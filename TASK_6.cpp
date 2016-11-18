0bc6af21b58e0226b2f494d95a79bc311fc86c55
Emmanuel Wang <Emmanuel Wang>
2016-11-19 01:00:49 +0800

    update

--- TASK_6.cpp
+++ TASK_6.cpp
@@ -4,5 +4,49 @@
  * don’t know the meaning of %s, %c, and %d. Use <cstdarg>.
  */
 
+#include <stdarg.h>
+#include <string>
+#include <iostream>
 
-int error()
+
+using namespace std;
+
+int error(const char *format, ...) {
+    string f = format;
+    va_list argp;
+    va_start(argp, format);
+
+    for (int i = 0; i < f.length(); i++) {
+        if (f[i] != '%') cout << f[i];
+        else {
+            switch (f[++i]) {
+                default:
+                    break;
+                case 'd': {
+                    int temp = va_arg(argp, int);
+                    cout << temp;
+                    break;
+                }
+                case 's': {
+                    string temp = va_arg(argp, string);
+                    cout << temp;
+                    break;
+                }
+                case 'c': {
+                    char temp = char(va_arg(argp, int));
+                    cout << temp;
+                    break;
+                }
+            }
+        }
+    }
+
+}
+
+
+int main() {
+    string a = "STRING";
+    error("fuck%d%s%c", 123,a,'c');
+    return 0;
+
+}
