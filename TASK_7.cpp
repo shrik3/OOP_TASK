808e15b5f428a82b9a235ee8cb94fb64c37d072c
Emmanuel Wang <Emmanuel Wang>
2016-11-19 09:35:50 +0800

    update

--- TASK_7.cpp
+++ TASK_7.cpp
@@ -20,7 +20,7 @@ struct date {
 };
 
 void IncreaseOneDay(date *d) {
-    if (d->day < d->index[d->month-1]) d->day++;
+    if (d->day < d->index[d->month - 1]) d->day++;
     else if (d->month < 12) {
         d->month++;
         d->day = 1;
@@ -32,30 +32,66 @@ void IncreaseOneDay(date *d) {
     }
 }
 
-void IncreaseOneMonth(date *d){
-    if (d->month <12){
+int isRunYear(int year) {
+    if (((year % 4 == 0) && ((year % 100 != 0)) || (year % 400 == 0))) return 1;
+    else return 0;
+}
+
+int WhichDay(date d) {               // 计算此日期是当年的第几天
+    int n = 0;
+    for (int i = 1; i < d.month; i++) {
+        n += d.index[i - 1];
+    }
+    n += d.day;
+    return n;
+}
+
+
+int DateMinus(date a, date b) {      // assuming a > b
+    int gap = 0;
+    gap += WhichDay(a) - WhichDay(b) + 365;
+    if (isRunYear(b.year)) gap += 1;
+    for (int i = b.year; i < a.year; i++) {
+        gap += 365 + isRunYear(i);
+    }
+    return gap;
+}
+
+int WhichWeekDay(date d) {
+    date stddate;
+    stddate.year = 1901;
+    stddate.month = 1;
+    stddate.day = 1;
+    stddate.init();
+    return (DateMinus(d, stddate) % 7) + 1;
+}
+
+
+void IncreaseOneMonth(date *d) {
+    if (d->month < 12) {
         d->month++;
-        if(d->day>d->index[d->month-1]) d->day = d->index[d->month-1];
-    }else{
-        d->year ++;
+        if (d->day > d->index[d->month - 1]) d->day = d->index[d->month - 1];
+    } else {
+        d->year++;
         d->init();
         d->month = 1;
-        if(d->day>d->index[d->month-1]) d->day = d->index[d->month-1];
+        if (d->day > d->index[d->month - 1]) d->day = d->index[d->month - 1];
     }
 }
 
-void IncreaseOneYear(date *d){
+void IncreaseOneYear(date *d) {
     d->year++;
     d->init();
-    if(d->day>d->index[d->month-1]) d->day = d->index[d->month-1];
+    if (d->day > d->index[d->month - 1]) d->day = d->index[d->month - 1];
 
 }
 
+
 int main() {
     date a;
-    a.year = 1999;
-    a.month = 1;
-    a.day = 30;
+    a.year = 2015;
+    a.month = 11;
+    a.day = 17;
     a.init();
     IncreaseOneDay(&a);
     cout << a.year << '/' << a.month << '/' << a.day << endl;
@@ -63,6 +99,8 @@ int main() {
     cout << a.year << '/' << a.month << '/' << a.day << endl;
     IncreaseOneYear(&a);
     cout << a.year << '/' << a.month << '/' << a.day << endl;
+
+    cout << WhichWeekDay(a);
     return 0;
 }
 
